237c58a8533865b690cdbecb39abef2ac218efc7
German Nunez <german90nunez@gmail.com>
2020-07-12 10:28:18 -0700

    service layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import transCars from "../images/trans-cars.png"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
@@ -17,7 +19,16 @@ const IndexPage = () => (
     </div>
 
     <div className="mission-statment">
-
+      <div className="statment-word-container">
+        <div className="statment-words">
+          <h2><span className="first-word">Our</span> commitment to you</h2>
+          <div className="underline"></div>
+        </div>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+      </div>
+      <div className="transparnt-image">
+        <img style={{height: '100%', width: '100%'}} src={transCars} />
+      </div>
     </div>
 
     <div className="service-section">
