f46f0a164393652c479d3092d5c916ae89bec470
German Nunez <german90nunez@gmail.com>
2020-07-16 14:56:43 -0700

    service section created

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,12 +61,19 @@ const IndexPage = () => (
     </div>
 
     <div className="service-section">
-
+      <div className="service-content">
+        <div className="service-text">
+          <h4>Lorem Ipsum</h4>
+          <div className="underline"></div>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
+      </div>
     </div>
 
 
     <div className="brands">
-
+      <div className="brand-content">
+      </div>
     </div>
 
     <div className="map-container">
