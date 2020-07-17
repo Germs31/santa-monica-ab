a9855d18704ef54ea56e9ec0657aebffb16b6ffa
German Nunez <german90nunez@gmail.com>
2020-07-16 21:55:14 -0700

    header is finsihed and responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React, {useState} from "react"
-// import logo from '../images/logo.png'
-import '../Header/Header.scss'
+import logo from '../../images/white-logo.png'
+import '../Header/Header.scss';
+import {MdLocationOn, MdPhone, MdAccessTime} from 'react-icons/md'
+
 
 const Header = ({ siteTitle }) => {
   const [isOpen, setIsOpen] = useState(false)
@@ -15,6 +17,7 @@ const Header = ({ siteTitle }) => {
     <header>
       <div className="nav-container">
         <div className="logo">
+          <img src={logo}/>
           <Link to="/">
             <h2>Santa Monica Auto body</h2> 
           </Link>
@@ -39,7 +42,27 @@ const Header = ({ siteTitle }) => {
         </div>
         <Hamburger setIsOpen={setIsOpen} isOpen={isOpen} />
       </div>
+      <div className="nav-contact">
+        <div className="hours">
+            <MdLocationOn className="nav-icon"/>
+            <p>Mon - Fri: 9Am - 6pm</p>
+        </div>
+        <div className="address">
+          <MdAccessTime className="nav-icon"/>
+          <a target="_blank" href="https://www.google.com/maps/place/5262+Washington+Blvd,+Los+Angeles,+CA+90016/">5262 Washington Blvd, Los Angeles, CA 90016</a>
+        </div>
+        <div className="phone">
+          <MdPhone className="nav-icon"/>
+          <a href="tel:+13239374064">(323) 937-4064</a>
+        </div>
+      </div>
       <div className={isOpen ? " mobile-overlay show" : "mobile-overlay hide"} onClick={() => setIsOpen(!isOpen)}>
+        <div class="overlay-contact">
+          <div className="overlay-contact-ctx">
+           <MdPhone className="overlay-icon"/>
+            <a href="tel:+13239374064">(323) 937-4064</a>
+          </div>
+        </div>
         <nav>
           <ul>
             <li>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-import {MdLocationOn, MdPhone, MdAccessTime} from 'react-icons/md'
 
 
 import Layout from "../components/layout"
