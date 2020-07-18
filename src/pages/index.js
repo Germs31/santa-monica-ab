23d0565dc218b792b128bedee2c95163d2efa9a2
German Nunez <german90nunez@gmail.com>
2020-07-17 18:26:16 -0700

    landing section responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,13 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="landing">
-      
+      <div className="landing-content">
+        <h1>Los Angeles Auto Collision & Paint</h1>
+        <h3>All insurances welcomed</h3>
+        <button>
+          <Link to="/contact">Free Estimate</Link>
+        </button>
+      </div>
     </div>
     <div className="mission-statment">
 
