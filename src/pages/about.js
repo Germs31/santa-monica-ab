8b7b4a2a8797fd2c4b0122301508e3a31e3ec4cd
German Nunez <german90nunez@gmail.com>
2020-07-19 18:45:28 -0700

    about section finished but not mobile

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,32 @@
 import React from 'react'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import BackShop from '../images/back-shop.jpg'
+import {Link} from 'gatsby'
+import '../pages/style/About/about.scss'
 const about = () => {
     return (
         <Layout>
             <SEO title="About"/>
-            <div className="about-page">
-                <div className="about-banner">
-                    <div className="about-banner-text">
-                        <h1>Our History</h1>
-                    </div>
+            <div className="about-banner">
+                <div className="about-banner-text">
+                    <h1>About Us</h1>
                 </div>
+            </div>
+            <div className="about-content">
                 <div className="about-text">
+                    <h3><span className="first-word">Our </span>History</h3>
                     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+
+                    <button>
+                        <Link to="/contact">Free Estimate</Link>
+                    </button>
+                </div>
+
+                <div className="about-content-img">
+                    <img src={BackShop}/>
                 </div>
             </div>
         </Layout>
