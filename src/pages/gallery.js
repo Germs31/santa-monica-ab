995c4168a3e4b5e8978fe3e9162ec6a046a7b1cc
German Nunez <german90nunez@gmail.com>
2020-07-22 12:56:10 -0700

    should deploy no errors now removed image from query

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -20,7 +20,6 @@ const gallery = ({ data} ) => {
             <div className="gallery-images">
                 <Img className="gallery-img" fluid={data.backShop.childImageSharp.fluid} />
                 <Img className="gallery-img" fluid={data.elCoDamage.childImageSharp.fluid} />
-                <Img className="gallery-img" fluid={data.rarriImage.childImageSharp.fluid} />
                 <Img className="gallery-img" fluid={data.detailSection.childImageSharp.fluid} />
                 <Img className="gallery-img" fluid={data.cornerSpray.childImageSharp.fluid} />
                 <Img className="gallery-img" fluid={data.backSpray.childImageSharp.fluid} />
@@ -34,63 +33,56 @@ const gallery = ({ data} ) => {
 
 export const query = graphql`
     {
-        backShop: file(relativePath: { eq: "back-shop.JPG" }) {
+        backShop: file(relativePath: { eq: "back-shop.jpg" }) {
             childImageSharp {
                 fluid {
                     ...GatsbyImageSharpFluid
                 }
             }
         }
-        elCoDamage: file(relativePath: { eq: "el-co-damage.JPG" }) {
+        elCoDamage: file(relativePath: { eq: "el-co-damage.jpg" }) {
             childImageSharp {
                 fluid {
                     ...GatsbyImageSharpFluid
                 }
             }
         }
-        rarriImage: file(relativePath: { eq: "rarri.JPG" }) {
+        detailSection: file(relativePath: { eq: "detail-section.jpg" }) {
             childImageSharp {
                 fluid {
                     ...GatsbyImageSharpFluid
                 }
             }
         }
-        detailSection: file(relativePath: { eq: "detail-section.JPG" }) {
+        cornerSpray: file(relativePath: { eq: "corner-spray.jpg" }) {
             childImageSharp {
                 fluid {
                     ...GatsbyImageSharpFluid
                 }
             }
         }
-        cornerSpray: file(relativePath: { eq: "corner-spray.JPG" }) {
+        frontShop: file(relativePath: { eq: "shop-front.jpg" }) {
             childImageSharp {
                 fluid {
                     ...GatsbyImageSharpFluid
                 }
             }
         }
-        frontShop: file(relativePath: { eq: "shop-front.JPG" }) {
+        frontRightShop: file(relativePath: { eq: "front-right-shop.jpg" }) {
             childImageSharp {
                 fluid {
                     ...GatsbyImageSharpFluid
                 }
             }
         }
-        frontRightShop: file(relativePath: { eq: "front-right-shop.JPG" }) {
+        backSpray: file(relativePath: { eq: "spray-room-back.jpg" }) {
             childImageSharp {
                 fluid {
                     ...GatsbyImageSharpFluid
                 }
             }
         }
-        backSpray: file(relativePath: { eq: "spray-room-back.JPG" }) {
-            childImageSharp {
-                fluid {
-                    ...GatsbyImageSharpFluid
-                }
-            }
-        }
-        middleShopTwo: file(relativePath: { eq: "middle-shop-two.JPG" }) {
+        middleShopTwo: file(relativePath: { eq: "middle-shop-two.jpg" }) {
             childImageSharp {
                 fluid {
                     ...GatsbyImageSharpFluid
