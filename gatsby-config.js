07f25434421ce5c0908f893ee5a1ccc3e9192e41
German Nunez <german90nunez@gmail.com>
2020-07-21 18:38:23 -0700

    removed google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,21 +8,6 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Roboto Mono`,
-            variants: [`400`, `700`]
-          },
-          {
-            family: `Roboto`,
-            subsets: [`latin`]
-          },
-        ],
-      },
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
